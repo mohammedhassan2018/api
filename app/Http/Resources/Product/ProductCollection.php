18825caf0adeea1cfb4112a477cdcb8a3f4138d3
Mohammed Hassan <eng.mohammed.hassan2017@gmail.com>
2018-05-29 17:41:38 +0200

    Get Review - API Resource
    Get Review - API Resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -6,12 +6,7 @@ use Illuminate\Http\Resources\Json\Resource;
 
 class ProductCollection extends Resource
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+
     public function toArray($request)
     {
         return [
--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -6,12 +6,7 @@ use Illuminate\Http\Resources\Json\Resource;
 
 class ProductResource extends Resource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+
     public function toArray($request)
     {
         return [
