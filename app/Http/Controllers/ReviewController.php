d57e0f95522665fd0fa3629bb1d47edc805dde58
Mohammed Hassan <eng.mohammed.hassan2017@gmail.com>
2018-05-31 00:13:41 +0200

    Complete Reviews
    Complete Reviews update & delete

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -41,13 +41,17 @@ class ReviewController extends Controller
         //
     }
 
-    public function update(Request $request, Review $review)
+    public function update(Request $request,Product $product,Review $review)
     {
-        //
+        $review->update($request->all());
+        return response([
+            'data' => new ReviewResource($review)
+        ],Response::HTTP_CREATED);
     }
 
-    public function destroy(Review $review)
+    public function destroy(Product $product,Review $review)
     {
-        //
+        $review->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
