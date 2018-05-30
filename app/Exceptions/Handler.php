303eba03ad0c89aaf220ceae9a93e77a423ac4f3
Mohammed Hassan <eng.mohammed.hassan2017@gmail.com>
2018-05-30 21:37:42 +0200

    Customized Exception Trait
    Customized Exception Trait

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,74 +1,35 @@
 <?php
 
 namespace App\Exceptions;
-
 use Exception;
+use App\Exceptions\ExceptionTrait;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
+    use ExceptionTrait;
+
     protected $dontReport = [
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
     protected $dontFlash = [
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
     public function report(Exception $exception)
     {
         parent::report($exception);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
     public function render($request, Exception $exception)
     {
         if($request->expectsJson())
         {
-            if($exception instanceof ModelNotFoundException)
-            {
-                return response()->json([
-                    'error'=> 'product model not found'
-                ],Response::HTTP_NOT_FOUND);
-            }
-
-            if($exception instanceof NotFoundHttpException)
-            {
-                return response()->json([
-                    'error'=> 'Incorrect route'
-                ],Response::HTTP_NOT_FOUND); 
-            }
+           return $this->apiException($request, $exception);
         } 
 
-        //dd($exception);
         return parent::render($request, $exception);
     }
 }
