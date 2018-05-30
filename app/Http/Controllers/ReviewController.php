736587a80fadc3c901cf0839cffbe0960335e121
Mohammed Hassan <eng.mohammed.hassan2017@gmail.com>
2018-05-30 23:55:08 +0200

    Review Created
    Review Created

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,6 +6,8 @@ use App\Http\Resources\ReviewResource;
 use App\Model\Review;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\ReviewRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReviewController extends Controller
 {
@@ -20,9 +22,13 @@ class ReviewController extends Controller
         //
     }
 
-    public function store(Request $request)
+    public function store(ReviewRequest $request, Product $product)
     {
-        //
+        $review = new Review($request->all());
+        $product->reviews()->save($review);
+        return response([
+            'data' => new ReviewResource($review)
+        ],Response::HTTP_CREATED);
     }
 
     public function show(Review $review)
